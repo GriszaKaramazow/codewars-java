fc16d2b1a607395303dc92e4a38fea21ae83cb58
Grzegorz Witkowski <grisza.karamazow@gmail.com>
2020-03-29 20:34:20 +0200

    6kyu Detect Pangram

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # codewars trainings solved with Java
+
+6kyu Detect Pangram
---
+++ src/main/java/PangramChecker.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class PangramChecker {
+
+    public boolean check(String sentence){
+        char[] alphabetArray = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+        List<Character> alphabetList = new ArrayList<>();
+        for (char letter : alphabetArray) {
+            alphabetList.add(letter);
+        }
+        char[] sentenceArray = sentence.toLowerCase().toCharArray();
+        List<Character> lettersList = new ArrayList<>();
+        for (char character : sentenceArray) {
+            if (alphabetList.contains(character) && !lettersList.contains(character)) {
+                lettersList.add(character);
+            }
+        }
+        return lettersList.size() == 26;
+    }
+
+}
